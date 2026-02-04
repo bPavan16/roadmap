4cf5036925ddd6f0f16b76dd64784aa54b66d505
bPavan16 <pavanbhakta163@gmail.com>
2026-02-04 14:38:04 +0530

    added dummy middleware

---
+++ 4-express/src/middlewares/auth.middleware.js
@@ -0,0 +1,7 @@
+export const protect = (req, res, next) => {
+
+  console.log(`hello this is a dummy middleware`);
+  console.log("middleware invoked")
+
+  next();
+};
--- 4-express/src/routes/user.route.js
+++ 4-express/src/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from "express";
-
+import { protect } from "../middlewares/auth.middleware.js";
 import { getAllUsers, getUserById ,deleteUserById,updateUserById} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/", getAllUsers);
-router.get("/id/:id", getUserById);
-router.put("/id/:id", updateUserById);
-router.delete("/id/:id", deleteUserById);
+router.get("/", protect,getAllUsers);
+router.get("/id/:id", protect,getUserById);
+router.put("/id/:id", protect,updateUserById);
+router.delete("/id/:id", protect,deleteUserById);
+
 
 export default router;
